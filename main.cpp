5a244361ba8fb28a8b591b93ad816c8a259e5bc4
stagingcodacytestuser <stagingcodacytestuser@codacy.com>
2026-01-09 16:18:16 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+
+#ifdef FOO
+#ifdef FOO // inner ifdef is considered redundant
+    void f();
+#endif
+#endif
+
+// Function to add two numbers
+int add(int num1, int num2) {
+    return num1 + num2;
+}
+
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    
+    int a, b;
+
+    std::cout << "Enter first number: ";
+    std::cin >> a;
+
+    std::cout << "Enter second number: ";
+    std::cin >> b;
+
+    std::cout << "The sum is: " << add(a, b) << std::endl;
+
     return 0;
 }
